8c20934b499be8d2552bc172db0a01122d78a0f9
albanx <albanx@gmail.com>
2017-12-11 00:00:22 +0100

    tests

--- index.js
+++ index.js
@@ -1,10 +1,83 @@
 import request from 'request';
 import Gdax from 'gdax';
+import mongo from 'mongodb';
+import Kraken from './providers/kraken';
 
-const websocket = new Gdax.WebsocketClient(['BTC-USD']);
-websocket.on('message', data => {
-  console.log('Received data', data);
-});
+const MONGO_DB_URL = 'mongodb://localhost:27017';
+const DB_NAME = 'crypto_coins';
+const COIN_TABLE = 'coins';
+const krakenApi = new Kraken('', '', {});
+const MARKETS = [];
+
+const openDB = async () => {
+    const client = await mongo.MongoClient.connect(MONGO_DB_URL);
+    const db = client.db(DB_NAME);
+    let collection = await db.collection(COIN_TABLE);
+    return {client, collection};
+};
+
+const getUTCDate = () => {
+    const d = new Date();
+    const leadingZero = (num) => `0${num}`.slice(-2);
+
+    const formatTime = (date) =>
+        [date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()]
+            .map(leadingZero)
+            .join(':');
+
+    return d.getUTCFullYear() + '-' +
+        d.getUTCMonth() + '-' +
+        d.getUTCDate() + ' ' +
+        formatTime(d);
+};
+
+const checkCoinPrice = async (name, market) => {
+    let {client, collection} = await openDB().catch(err => console.log('Error ', err));
+    let coin = await collection.findOne({name, market});
+    console.log('checkCoinPrice::', coin);
+    client.close();
+};
+
+
+const updateCoinPrice = async (price, market, time) => {
+    console.log('updateCoinPrice::', price, market);
+    let {client, collection} = await openDB().catch(err => console.log('Error ', err));
+    collection.createIndex( { name: 1, market: 1 }, { unique: true } )
+
+    collection.update({
+        'name': 'BTC',
+        'market': 'kraken'
+    }, {
+        'name': 'BTC',
+        'market': market,
+        'price': price,
+        'time': time
+    }, {
+        upsert: true
+    });
+    client.close();
+    console.log('updateCoinPrice::done');
+};
+
+const callKrakenApi = async () => {
+    console.log('callKrakenApi::');
+    const data = await krakenApi.api('Ticker', { pair : 'XXBTZEUR' });
+    const price = data.result['XXBTZEUR'].b[0];
+    console.log('callKrakenApi::', price);
+
+    updateCoinPrice(price, 'kraken', getUTCDate()).catch(err => {
+        console.log('Cannot update price ', err);
+    })
+};
+
+checkCoinPrice('BTC', 'kraken');
+callKrakenApi();
+
+
+// const websocket = new Gdax.WebsocketClient(['BTC-USD']);
+// websocket.on('message', data => {
+//   console.log('Received data', data);
+// });
 
 // const API_END_POINT = 'https://api.gdax.com';
 
